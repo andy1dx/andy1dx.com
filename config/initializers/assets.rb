dc8e9d8bdd24feb785231b128701ae1c93a9950e
USER <andy.haz3@gmail.com>
2019-08-28 19:01:40 +0900

    Import template

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,9 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( template/main.js )
+Rails.application.config.assets.precompile += %w( template/plugins.js )
+Rails.application.config.assets.precompile += %w( template/jquery-3.2.1.min.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
