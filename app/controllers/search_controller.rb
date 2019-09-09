95f2cef06f793e1231205a89bbe95bfdb11351f1
USER <andy.haz3@gmail.com>
2019-09-09 14:11:30 +0900

    delete auth

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,5 @@
 class SearchController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def show
   	@keyword = params[:keyword]
   	@articles = Article.where(status: 1).where("title like '%#{@keyword}%'").order(publish_date: 'DESC').page(params[:page]).per(15)
